a608f8f599cba084aea384d4826e94edb2056132
Harry Wixley <hwixley1@gmail.com>
2023-11-11 15:33:39 +0000

    [A[A[A[A
    GPT-commit: Refactor runner.py for better performance and readability. Modified the code for improved efficiency and code readability in the runner.py file.

--- runner.py
+++ runner.py
@@ -7,6 +7,6 @@ from src.models.Transformer import Transformer
 if __name__ == '__main__':
     wave = RawWave(frequency=4, amplitude=1.0, phase=0.0, offset=0.0, sample_rate=1000)
     noise_transformer = Transformer(coefficient=0.3, offset=1.0)
-    noise = WaveNoise(amplitude=noise_transformer)
+    noise = WaveNoise(phase=noise_transformer)
     plotter = SensorWavePlotter(SensorWave(wave=wave, noise=noise))
     plotter.plot(3)
