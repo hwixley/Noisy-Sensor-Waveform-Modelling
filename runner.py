87f57891d3892f06c97791878391fe449f87a9de
Harry Wixley <hwixley1@gmail.com>
2023-11-12 12:16:13 +0000

    GPT-commit: Modified runner.py, SensorWavePlotter.py, SensorWavePlotter.cpython-38.pyc and waves.cpython-38.pyc for committing changes.
    GPT-commit: Modified runner.py, SensorWavePlotter.py, SensorWavePlotter.cpython-38.pyc and waves.cpython-38.pyc for committing changes to update functionality of specific functions, classes or variables.

--- runner.py
+++ runner.py
@@ -1,11 +1,11 @@
 from src.SensorWavePlotter import SensorWavePlotter
 from src.models.SensorWave import SensorWave
 from src.data.wave_noises import wave_noise1
-from src.data.waves import wave1
+from src.data.waves import wave1, wave2
 
 if __name__ == '__main__':
     sensor1 = SensorWave(wave=wave1, noise=wave_noise1)
-    sensor2 = SensorWave(wave=wave1, noise=wave_noise1)
+    sensor2 = SensorWave(wave=wave2, noise=wave_noise1)
     plotter = SensorWavePlotter(sensors=[sensor1, sensor2])
 
     plotter.plot(2,150)
--- src/SensorWavePlotter.py
+++ src/SensorWavePlotter.py
@@ -12,7 +12,7 @@ class SensorWavePlotter:
     def plot(self, N: int, M: int = 1000):
         color = iter(plt.cm.rainbow(np.linspace(0, 1, (N+1)*len(self.sensors))))
 
-        for i, sensor in self.sensors:
+        for i, sensor in enumerate(self.sensors):
             plt.plot(*sensor.get_raw_data(M), color=next(color), label=f'Sensor {i} Truth')
             for j in range(N):
                 plt.plot(*sensor.get_noise_data(M), color=next(color), label=f'Sensor {i} Reading {j}')
